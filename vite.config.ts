43a2864837e88917280e50a70503b21494f08583
Kamlesh Paul <kamleshpaul414@gmail.com>
2026-04-06 17:37:06 +0530

    feat: enhance Vite configuration with Tailwind CSS and enable prerendering for TanStack

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      prerender: {
+        // Enable prerendering
+        enabled: true,
+      }
+    }),
     viteReact(),
   ],
 })
